3ec80a8c73406dda6a17157b1c46730217ba827d
hoergems <hoergems@gmail.com>
2016-02-28 18:10:20 +1000

    Removed uneccessary creation of collision object

--- src/MotionValidator.cpp
+++ src/MotionValidator.cpp
@@ -26,9 +26,7 @@ MotionValidator::MotionValidator(const ompl::base::SpaceInformationPtr &si,
 
 bool MotionValidator::checkMotion(const std::vector<double> &s1, 
                                   const std::vector<double> &s2, 
-                                  const bool &continuous_collision) const {		
-	std::vector<std::shared_ptr<fcl::CollisionObject>> collision_objects_goal;
-	robot_->createRobotCollisionObjects(s2, collision_objects_goal);
+                                  const bool &continuous_collision) const {
     if (continuous_collision) {    	
     	return !collidesContinuous(s1, s2); 
     } 
