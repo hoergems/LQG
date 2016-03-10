cd0721892728a2509e8668eff44be948144543bc
hoergems <hoergems@gmail.com>
2016-03-10 17:47:40 +1000

    Added a clone script

---
+++ do.py
@@ -0,0 +1,17 @@
+import sys
+import os
+import subprocess
+import glob
+
+shared_path = os.path.dirname(os.path.abspath(__file__))
+
+for i in xrange(1, 30):
+    cmd = "cp LQG" + str(i) + "/config_hrf.yaml LQG" + str(i) + "/config_hrf.yamll"
+    popen = subprocess.Popen(cmd, cwd=shared_path, shell=True)
+    popen.wait()
+    
+    cmd = "cd LQG" + str(i) + " && git checkout -- config_hrf.yaml && git pull origin perf && cd .."
+    popen = subprocess.Popen(cmd, cwd=shared_path, shell=True)
+    popen.wait()
+    
+    cmd = "mv LQG" + str(i) + "/config_hrf.yamll LQG" + str(i) + "/config_hrf.yaml" 
